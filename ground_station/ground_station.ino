693c69202d6b45ba85e275f03919441c184eea11
Halil Yaman <40357552+halilyaman@users.noreply.github.com>
2019-08-03 00:30:28 +0300

    ground_station is added.

---
+++ ground_station/ground_station.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee(10, 11); //(RX, TX)
+char val;
+bool isMessage = false;
+
+
+void setup() {
+  Serial.begin(115200);
+  xbee.begin(9600);
+}
+
+void loop() {
+  while(xbee.available()) {
+    val = xbee.read();
+    if(val == '?') {
+      isMessage = true;
+    }
+    
+    if(val == '!') {
+      isMessage = false;
+    }
+
+    if(isMessage) {
+      if(val != '?') Serial.print(val);
+    }
+    
+  }
+}
